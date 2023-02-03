3e0604040675904a7401779760d48b9a76af80e6
Justin Huth <Jayhuth96@gmail.com>
2023-02-03 14:21:34 -0600

    finished both excercises

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -8,22 +8,64 @@
             
             var r = new Random();
             var favNumber = r.Next(1, 5);
+            int userInput;
+            var counter = 0;                           
 
-            Console.WriteLine("Enter in a number from 1-5.");
-            var userInput = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Welcome to my number guessing game!"); 
+                Console.WriteLine("Guess a number from 1-25.");
+                userInput = int.Parse(Console.ReadLine());
 
-            if (userInput < favNumber)
-            {
-                Console.WriteLine($"{userInput} is too low");
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too low");
 
-            }
-            else if (userInput > favNumber)
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too high:");
+                }
+                else
+                {
+                    Console.WriteLine("you got it!!!");
+                }
+
+                counter++;
+                Console.WriteLine($"you have guessed {counter} times");
+
+            } while (userInput != favNumber);
+
+            //Switch-Case
+            Console.WriteLine("What is your favorite subject?");
+
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower()) 
             {
-                Console.WriteLine($"{userInput} is too high:");
-            }
-            else 
-            {
-                Console.WriteLine("you got it!!!");
+                case "math":
+                case "calculus":
+                case "algerbra":
+                    Console.WriteLine("Math Sucks!!!!");
+                    break;
+                case "science":
+                case "physics":
+                case "biology":
+                    Console.WriteLine("science is pretty intresting.");
+                    break;
+                case "pe":
+                case "physical ed":
+                    Console.WriteLine("PE is fire!");
+                    break;
+                case "history":
+                    Console.WriteLine("History makes me sleepy.");
+                    break;
+                case "english":
+                    Console.WriteLine("English is too easy.");
+                    break;
+                default:
+                    Console.WriteLine("I am not aware of that subject.");
+                    break;
             }
         }
     }
