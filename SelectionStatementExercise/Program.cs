b9e2f500a4739cff7ce1fbbacecc222d176df5d2
Justin Huth <Jayhuth96@gmail.com>
2023-02-03 12:04:08 -0600

    finished excersise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Number Guessing Game
+            
+            var r = new Random();
+            var favNumber = r.Next(1, 5);
+
+            Console.WriteLine("Enter in a number from 1-5.");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too low");
+
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"{userInput} is too high:");
+            }
+            else 
+            {
+                Console.WriteLine("you got it!!!");
+            }
         }
     }
 }
